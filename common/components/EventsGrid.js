9b18ab19492324aedb6f16eb155555ff10b6d99c
Mohan-Personal <mohanprasaath269@gmail.com>
2023-03-15 02:46:41 +0530

    change

--- common/components/EventsGrid.js
+++ common/components/EventsGrid.js
@@ -59,23 +59,23 @@ function EventsGrid() {
         </Box>
 
         <Box display="flex" alignItems="center" justifyContent="center" width="100%">
-        <Grid container spacing={4} mt={4} width="100%">
-          <Grid item xs={12} md={4}  >
+        <Grid container spacing={4} mt={4} width="100%" >
+          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="120" display="flex" alignItems="center" justifyContent="center" width="100%">
             <EventCard />
           </Grid>
-          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="120">
+          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="120" display="flex" alignItems="center" justifyContent="center" width="100%">
             <EventCard />
           </Grid>
-          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="140">
+          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="140" display="flex" alignItems="center" justifyContent="center" width="100%">
             <EventCard />
           </Grid>
-          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="160" sx={{display:{xs:cond?"block":"none",md:"block"}}}>
+          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="160" sx={{display:{xs:cond?"block":"none",md:"block"}}} display="flex" alignItems="center" justifyContent="center" width="100%">
             <EventCard />
           </Grid>
-          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="180" sx={{display:{xs:cond?"block":"none",md:"block"}}}>
+          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="180" sx={{display:{xs:cond?"block":"none",md:"block"}}} display="flex" alignItems="center" justifyContent="center" width="100%">
             <EventCard />
           </Grid>
-          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="200" sx={{display:{xs:cond?"block":"none",md:"block"}}}>
+          <Grid item xs={12} md={4} data-aos="fade-right" data-aos-delay="200" sx={{display:{xs:cond?"block":"none",md:"block"}}} display="flex" alignItems="center" justifyContent="center" width="100%">
             <EventCard />
           </Grid>
         </Grid>
